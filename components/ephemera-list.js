dd3bd100c5a412ff0540884c9585f25bb4fc39ef
K Swa <kpswanson@gatech.edu>
2021-05-30 23:57:23 -0400

    releases update
    - added ephemera list
        - component w/ source
        - added id item to template for render
        - added sample images
    - updated releases
        - bandcamp embeds now load
        - using checkbox toggle with javascript for inner elements

---
+++ components/ephemera-list.js
@@ -0,0 +1,35 @@
+import {html, render} from 'https://unpkg.com/lit-html@latest?module';
+import {unsafeHTML} from 'https://unpkg.com/lit-html@latest/directives/unsafe-html.js?module';
+import {getEphemera} from './enmossed-sources.js';
+
+let source = getEphemera();
+
+const imageTemplate = (content) => html`
+<img height="290" width="290" src="${content}"></a>
+`;
+
+const descriptionTemplate = (description) => html`
+<p>
+${description}
+</p>
+`;
+
+const ephemeraTemplate = (title, ephemeraObj) => html`
+<div>
+<label for="toggle-${title}">${title}</label>
+<input id="toggle-${title}" type="checkbox"></input>
+<div>
+${imageTemplate(ephemeraObj.src)}
+${descriptionTemplate(ephemeraObj.description)}
+</div>
+</div>
+`;
+
+const template = () => html`
+${Object.keys(source).filter((label) => source.hasOwnProperty(label)).map((label) => ephemeraTemplate(label, source[label]))}
+`;
+
+export default function ephemeraListTemplate(templateElementSelector) {
+    let node = document.querySelector(templateElementSelector);
+    render(template(), node);
+};
--- components/release-tree.js
+++ components/release-tree.js
@@ -15,25 +15,81 @@ ${description}
 `;
 
 const releaseTemplate = (releaseObj) => html`
-<details>
-<summary>${releaseObj.title}</summary>
+<div>
+<label for="toggle-${releaseObj.title}">${releaseObj.title}</label>
+<input class="child" type="checkbox" id="toggle-${releaseObj.title}">
+<div>
 ${embeddedPlayerTemplate(releaseObj.embed)}
 ${descriptionTemplate(releaseObj.description)}
-</details>
+</div>
+</div>
 `;
 
 const seriesTemplate = (title, series) => html`
-<details>
-<summary>${title}</summary>
+<div>
+<label for="toggle-${title}">${title}</label>
+<input class="parent" type="checkbox" id="toggle-${title}">
+<div>
 ${series.map((release) => releaseTemplate(release))}
-</details>
+</div>
+</div>
 `;
 
 const template = () => html`
 ${Object.keys(source).filter((label) => source.hasOwnProperty(label)).map((label) => seriesTemplate(label, source[label]))}
 `;
 
-export default function releaseTree(templateElementSelector) {
+export function ReleaseTree(templateElementSelector) {
     let node = document.querySelector(templateElementSelector);
     render(template(), node);
+};
+
+const toggleLookup = {};
+
+const toggleFromLookup = (childToggles) => {
+    childToggles.forEach((foundToggleId) => {
+        let toggle = document.getElementById(foundToggleId);
+        toggle.checked = false;
+    });
+};
+
+const getReleaseToggle = (parentId, child) => {
+    let releaseToggle = Array.from(child.children).filter((element) => {
+        return element.classList.contains("child");
+    });
+
+    if (releaseToggle && releaseToggle.length > 0) {
+        releaseToggle = releaseToggle[0];
+
+        toggleLookup[parentId].push(releaseToggle.id);
+
+        return releaseToggle;
+    }
+
+    return [];
+};
+
+const onSeriesToggle = (parent) => {
+    if (parent.checked) {
+        return;
+    }
+
+    if (toggleLookup[parent.id]) {
+        toggleFromLookup(toggleLookup[parent.id]);
+        return;
+    }
+
+    toggleLookup[parent.id] = [];
+
+    Array.from(parent.nextElementSibling.children).map((child) => {
+        return getReleaseToggle(parent.id, child);
+    }).forEach(function (toggle) {
+        toggle.checked = false;
+    });
+};
+
+export function CollapseChildren() {
+    [...document.querySelectorAll('.parent')].forEach((parent) => {
+        parent.addEventListener('change', () => onSeriesToggle(parent));
+    });
 };
